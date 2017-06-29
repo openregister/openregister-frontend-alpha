a61b1d8be837624da353fa76ad312319efc7bd19
Dan Mitchell <dan@digitalreflow.co.uk>
2017-06-29 11:16:22 +0100

    Render all beta registers on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @registers = OpenRegister.register('register', :beta)._all_records.sort_by(&:key)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def prepare_register_name(register_name)
+    unless register_name.nil?
+      register_name.gsub('-', ' ').split.map(&:capitalize) * ' '
+    end
+  end
+
+  def get_register(register_name)
+    OpenRegister.register(register_name.downcase, :beta)
+  end
 end
