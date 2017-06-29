d276e044362e06943262fe507ea94c8e9b35a599
Dan Mitchell <dan@digitalreflow.co.uk>
2017-06-29 16:39:50 +0100

    Add authority and desc to register page
    This commit also add the learn more links and information about
    registers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,29 @@ module ApplicationHelper
   def get_register(register_name)
     OpenRegister.register(register_name.downcase, :beta)
   end
+
+  def crest_class_name(authority)
+    case authority
+    when "home-office"
+      "logo-with-crest crest-ho"
+    when "ministry-of-defence"
+      "logo-with-crest crest-mod"
+    when "hm-revenue-customs"
+      "logo-with-crest crest-hmrc"
+    when "welsh-government"
+      "logo-with-crest crest-wales"
+    when "department-for-business-innovation-and-skills"
+      "logo-with-crest crest-bis"
+    when "the-office-of-the-leader-of-the-house-of-commons"
+      "logo-with-crest crest-portcullis"
+    when "office-of-the-advocate-general-for-scotland"
+      "logo-with-crest crest-so"
+    when "uk-atomic-energy-authority"
+      "logo-with-crest crest-ukaea"
+    when "nhs", "government-digital-service", "scottish-government"
+      " "
+    else
+      "logo-with-crest crest-org"
+    end
+  end
 end
