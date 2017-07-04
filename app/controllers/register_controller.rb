2309c38c43210502cc203eb18754aa93d1a316df
Dan Mitchell <dan@digitalreflow.co.uk>
2017-07-04 12:00:19 +0100

    Remove records into partial

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -4,7 +4,7 @@ class RegisterController < ApplicationController
   include ApplicationHelper
 
   def index
-    @register_records = HTTParty.get("https://#{params[:register]}.#{params[:phase]}.openregister.org/records.json?page-size=5000", headers: { 'Content-Type' => 'application/json' } )
+    @records = HTTParty.get("https://#{params[:register]}.#{params[:phase]}.openregister.org/records.json?page-size=5000", headers: { 'Content-Type' => 'application/json' } )
     @register_meta_data = HTTParty.get("https://#{params[:register]}.#{params[:phase]}.openregister.org/register.json", headers: { 'Content-Type' => 'application/json' } )
 
     @all_fields = HTTParty.get("https://field.register.gov.uk/records.json", headers: { 'Content-Type' => 'application/json' } )
