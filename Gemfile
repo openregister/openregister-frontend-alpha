e9935feb85ea618aadf6f67d5f4eba1eca4dd2d1
Dan Mitchell <dan@digitalreflow.co.uk>
2017-07-05 20:20:01 +0100

    Add openregister gem and setup filtering

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'haml-rails'
 gem 'govuk_template'
 gem 'govuk_frontend_toolkit'
 gem 'govuk_elements_rails'
+gem 'openregister-ruby', git: 'https://github.com/openregister/openregister-ruby-client'
 gem 'httparty'
 
 group :development do
--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -4,7 +4,14 @@ class RegisterController < ApplicationController
   include ApplicationHelper
 
   def index
-    @records = HTTParty.get("https://#{params[:register]}.#{params[:phase]}.openregister.org/records.json?page-size=5000", headers: { 'Content-Type' => 'application/json' } )
+    @records = OpenRegister.register(params[:register], params[:phase].to_sym)._all_records
+
+    if params[:current] == 'true'
+      @records = @records.select{ |x| x.end_date.blank? }
+    elsif params[:current] == 'false'
+      @records = @records.select{ |x| x.end_date.present? }
+    end
+
     @register_meta_data = HTTParty.get("https://#{params[:register]}.#{params[:phase]}.openregister.org/register.json", headers: { 'Content-Type' => 'application/json' } )
 
     @all_fields = HTTParty.get("https://field.register.gov.uk/records.json", headers: { 'Content-Type' => 'application/json' } )
